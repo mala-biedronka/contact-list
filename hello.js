ab8b2cb59f5454f242ce2051437eade9db62e385
Iryna Predborska <iryna.predborska@gmail.com>
2019-11-26 11:41:38 +0100

    Initial code.

---
+++ hello.js
@@ -0,0 +1,21 @@
+const contactList = [
+    {
+        firstName: 'Paulina',
+        lastName: 'Goldenvur',
+        phoneNumber: '828-493-123',
+        emailAddress: 'sunflower@gmail.com'
+    },
+    {
+        firstName: 'Matiusz',
+        lastName: 'Pochevsky',
+        phoneNumber: '382-032-419',
+        emailAddress: 'code@gmail.com'
+    }
+];
+
+
+
+
+
+
+console.log();
